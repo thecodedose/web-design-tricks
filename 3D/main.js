0850e736052dc23e7cfe2702c2497702176a0818
ihsavru <vurvashi11@gmail.com>
2024-11-12 17:54:49 +0530

    Add spline viewer example:

--- 3D/main.js
+++ 3D/main.js
@@ -16,35 +16,21 @@ camera.lookAt(0, 0, 0)
 const ambientLight = new THREE.AmbientLight(0xffffff, 1)
 scene.add(ambientLight)
 
-// Add directional light to simulate sunlight
-
 const modelLoader = new THREE.GLTFLoader()
 let model
-const joeyArmy = new THREE.Group()
 modelLoader.load(
   "./concrete-shape.glb",
   function (gltf) {
-    // Add the loaded model to the scene
+    // Load the model once and add it to the scene
     model = gltf.scene
     model.traverse((child) => {
       if (child instanceof THREE.Mesh) {
         child.material = new THREE.MeshNormalMaterial()
       }
     })
-    for (let i = 0; i < 20; i++) {
-      const c = model.clone()
-      c.rotation.y = Math.PI / 2
-      c.scale.set(0.4 + i * 0.01, 0.4 + i * 0.01, 0.4 + i * 0.01)
-      if (i % 2 === 0) {
-        c.position.set(i * 0.1, i * 0.1, 0)
-      } else {
-        c.position.set(i * -0.1, i * -0.1, 0)
-      }
-
-      joeyArmy.add(c)
-    }
-
-    scene.add(joeyArmy)
+    model.rotation.y = Math.PI / 2
+    model.scale.set(0.5, 0.5, 0.5) // Adjust scale as needed
+    scene.add(model)
   },
   undefined,
   function (error) {
@@ -72,14 +58,8 @@ function animate() {
     targetRotationX = mouseY * Math.PI * 2 // Rotate based on mouse Y position
     targetRotationY = mouseX * Math.PI * 2 // Rotate based on mouse X position
 
-    let i = 0
-    while (i < joeyArmy.children.length) {
-      joeyArmy.children[i].rotation.x +=
-        (targetRotationX - joeyArmy.children[i].rotation.x) * 0.05
-      joeyArmy.children[i].rotation.y +=
-        (targetRotationY - joeyArmy.children[i].rotation.y) * 0.05
-      i++
-    }
+    model.rotation.x += (targetRotationX - model.rotation.x) * 0.05
+    model.rotation.y += (targetRotationY - model.rotation.y) * 0.05
   }
 
   renderer.render(scene, camera)
